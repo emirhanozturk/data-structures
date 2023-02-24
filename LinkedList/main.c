23da4c987de19da0e057f2cf230ab190e11952be
Emirhan Öztürk <68188573+emirhanozturk@users.noreply.github.com>
2023-02-24 18:29:08 +0300

    LinkedList delete data function has written

--- LinkedList/main.c
+++ LinkedList/main.c
@@ -23,6 +23,7 @@ void printer(node* r){
 		printf(" %d ",r->x);
 		r=r->next;
 	}
+	printf(" \n ");
 }
 
 node* addSequential(node* r,int x){
@@ -43,6 +44,7 @@ node* addSequential(node* r,int x){
 	while(iter->next != NULL && iter->next->x < x){
 		iter=iter->next;
 	}
+
 	node* temp = (node*)malloc(sizeof(node));
 	temp->next = iter->next;
 	iter->next = temp;
@@ -51,13 +53,56 @@ node* addSequential(node* r,int x){
 	
 }
 
+node* delete(node* r,int x){
+	node* iter = r;
+	node* temp;
+	if(r->x == x){
+		temp = r;
+		r=r->next;
+		free(temp);
+		return r;
+	}
+	while(iter->next!=NULL && iter->next-> x !=x ){
+		iter=iter->next;
+	}
+	if(iter->next==NULL){
+		printf("Sayi bulunamadi \n");
+		return r;
+	}
+	temp = iter->next;
+	iter->next = iter->next->next;
+	free(temp);
+	return r;
+}
+
 int main(int argc, char *argv[]) {
 	node* root;
 	root = NULL;
 	root = addSequential(root,400);
 	root = addSequential(root,25);
 	root = addSequential(root,10);
+	printer(root);
+	root = delete(root,25);
 	root = addSequential(root,60);
+	printer(root);
+	root = delete(root,10);
+	root = delete(root,10);
+	printer(root);
+	root = addSequential(root,22);
+	printer(root);
+	root = addSequential(root,35);
+	printer(root);
+	root = addSequential(root,45);
+	printer(root);
+	root = addSequential(root,20);
+	printer(root);
+	root = addSequential(root,99);
+	printer(root);
+	root = delete(root,99);
+	printer(root);
+	root = delete(root,99);
+	printer(root);
+	root = delete(root,35);
 	printer(root);
 	
 	
